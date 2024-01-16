d3838687cd542e4c12043972a2bf4204be881e81
jorge <jomare1188@gmail.com>
2024-01-15 22:19:29 -0200

    changue variable names

--- format_interpo_for_conekt.sh
+++ format_interpo_for_conekt.sh
@@ -4,7 +4,7 @@ INTERPRO_FILE=Scp1.aa.nonStop.interpro.tsv.gz
 # pls see https://interproscan-docs.readthedocs.io/en/latest/OutputFormats.html to kwon wich columns to cut
 # sed 's/(InterPro)//g' and sed 's/(PANTHER)//g' is specific to the GO structure in interprofile, maybe if you want another column you dont need this 
 
-zcat Scp1.aa.nonStop.interpro.tsv.gz | cut -f1,14 | sed "s/|/,/g" | sed 's/(InterPro)//g' | sed 's/(PANTHER)//g' > id_GO_tmp.tsv
+zcat $INTERPRO_FILE | cut -f1,14 | sed "s/|/,/g" | sed 's/(InterPro)//g' | sed 's/(PANTHER)//g' > id_GO_tmp.tsv
 # separate GO in multiples rows in R
 ## need tidyr https://tidyr.tidyverse.org 
 Rscript separate_rows.r
--- separate_rows.r
+++ separate_rows.r
@@ -4,5 +4,5 @@ df2 <- df[complete.cases(df),]
 df2$V3 <- "ISM"
 df3 <- separate_rows(df2, V2, sep = ",")
 df4 <- unique(df3)
-write.table(df4, "GO_for_conekt_test.tsv", quote = F, row.names = F, col.names = F)
+write.table(df4, "GO_for_conekt.tsv", quote = F, row.names = F, col.names = F)
 
